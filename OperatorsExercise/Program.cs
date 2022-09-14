7c2971ca7f16b6a9c4c3fbe62b5ad275d50c939c
Bdotgo17 <bdotgo@hotmail.com>
2022-09-14 17:46:17 -0500

    initial

---
+++ OperatorsExercise/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace OperatorsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // exercise 1 ---------------
+
+            var a = 17;
+
+            var b = 4;
+
+            var div = a / b;
+            var mod = a % b;
+
+            Console.WriteLine($"{a}/{b} is {div} remainder {mod}");
+
+            // exercise 2 ---------------
+
+            var r = 20;
+            var pi = Math.PI;
+
+            var areaOfCircle = pi * (r * r);
+
+            Console.WriteLine($"The area of a circle with a radius of {r} is {areaOfCircle}");
+
+
+        }
+    }
+}
+
